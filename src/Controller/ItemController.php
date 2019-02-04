719c59590287e3e4c52b6f0ec5198414be849849
Josh Freeman <josh@viion.co.uk>
2019-02-04 23:28:30 +0000

    More style improvements

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -2,15 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\Alert;
 use App\Services\Cache\Cache;
 use App\Services\Companion\Companion;
 use App\Services\Companion\CompanionCensus;
 use App\Services\GameData\GameServers;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
+    /** @var EntityManagerInterface */
+    private $em;
     /** @var Cache */
     private $cache;
     /** @var Companion */
@@ -18,8 +22,9 @@ class ProductController extends AbstractController
     /** @var Companion */
     private $companionCensus;
     
-    public function __construct(Cache $cache, Companion $companion, CompanionCensus $companionCensus)
+    public function __construct(EntityManagerInterface $em, Cache $cache, Companion $companion, CompanionCensus $companionCensus)
     {
+        $this->em = $em;
         $this->cache = $cache;
         $this->companion = $companion;
         $this->companionCensus = $companionCensus;
@@ -63,6 +68,8 @@ class ProductController extends AbstractController
             // temp cache to avoid slow load times to prod servers
             file_put_contents(__DIR__.'/temp.json', json_encode($data));
         }
+    
+        $data['alerts'] = $this->em->getRepository(Alert::class)->findBy([ 'itemId' => $itemId ]);
         
         return $this->render('Product/index.html.twig', $data);
     }
