44a5e7782ad921f6899ca0f2d9171430f7b1608b
Josh Freeman <josh@viion.co.uk>
2019-04-03 13:14:41 +0100

    Update to new discord bot logic

--- src/Service/UserAlerts/UserAlertsDiscordNotification.php
+++ src/Service/UserAlerts/UserAlertsDiscordNotification.php
@@ -6,6 +6,7 @@ use App\Entity\UserAlert;
 use App\Service\Common\Mog;
 use App\Service\Common\SerAymeric;
 use App\Service\Redis\Redis;
+use App\Service\ThirdParty\Discord\Discord;
 
 class UserAlertsDiscordNotification
 {
@@ -42,7 +43,7 @@ class UserAlertsDiscordNotification
             'fields'        => $conditions,
         ];
 
-        SerAymeric::sendEmbed($embed, $alert->getUser()->getSsoDiscordId());
+        Discord::seraymeric()->sendEmbed($embed, $alert->getUser()->getSsoDiscordId());
     }
 
     /**
@@ -59,6 +60,6 @@ class UserAlertsDiscordNotification
             'footer'        => self::FOOTER,
         ];
 
-        SerAymeric::sendEmbed($embed, $alert->getUser()->getSsoDiscordId());
+        Discord::seraymeric()->sendEmbed($embed, $alert->getUser()->getSsoDiscordId());
     }
 }
