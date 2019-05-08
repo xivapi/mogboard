e2526b1719573f21998bfe19332e3d676667dc8c
Josh Freeman <josh@viion.co.uk>
2019-05-08 21:41:19 +0100

    a thing

--- src/Command/Ratchet/RatchetCommand.php
+++ src/Command/Ratchet/RatchetCommand.php
@@ -7,6 +7,9 @@ use App\Service\Ratchet\RatchetService;
 use Ratchet\Server\IoServer;
 use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
+use React\EventLoop\Factory;
+use React\Socket\SecureServer;
+use React\Socket\Server;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -23,24 +26,29 @@ class RatchetCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $output->writeln("Running ratchet client...");
-        
-        $app = new HttpServer(
-            new WsServer(
-                new RatchetService()
+    
+        $loop   = Factory::create();
+        $webSock = new SecureServer(
+            new Server('0.0.0.0:8090', $loop),
+            $loop,
+            array(
+                'local_cert'        => '/etc/letsencrypt/archive/mogboard.com/cert1.pem', // path to your cert
+                'local_pk'          => '/etc/letsencrypt/archive/mogboard.com/privkey1.pem', // path to your server private key
+                'allow_self_signed' => TRUE, // Allow self signed certs (should be false in production)
+                'verify_peer' => FALSE
             )
         );
-    
-        $loop = \React\EventLoop\Factory::create();
-    
-        $secure_websockets = new \React\Socket\Server('0.0.0.0:8081', $loop);
-        $secure_websockets = new \React\Socket\SecureServer($secure_websockets, $loop, [
-            'local_cert'  => '/etc/letsencrypt/live/mogboard.com/cert.pem',
-            'local_pk'    => '/etc/letsencrypt/live/mogboard.com/privkey.pem',
-            'verify_peer' => false,
-            'allow_self_signed' => true
-        ]);
         
-        $secure_websockets_server = new \Ratchet\Server\IoServer($app, $secure_websockets, $loop);
-        $secure_websockets_server->run();
+        // Ratchet magic
+        $webServer = new IoServer(
+            new HttpServer(
+                new WsServer(
+                    new RachetService()
+                )
+            ),
+            $webSock
+        );
+        
+        $loop->run();
     }
 }
