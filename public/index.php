16693682d5600a0fb00cd4d5abe61e7033935366
Josh Freeman <josh@viion.co.uk>
2019-05-12 18:39:29 +0100

    Maintenance Check

--- public/index.php
+++ public/index.php
@@ -6,6 +6,11 @@ use Symfony\Component\HttpFoundation\Request;
 
 require dirname(__DIR__).'/config/bootstrap.php';
 
+// Maintenance Check
+require_once __DIR__.'/../src/Common/Maintenance/MaintenanceCheck.php';
+$maintenance = new \App\Common\Maintenance\MaintenanceCheck();
+$maintenance->check();
+
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
 
