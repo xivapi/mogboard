986cd25aac846eca10a991a928864f323fc78b27
Josh Freeman <josh@viion.co.uk>
2019-06-17 10:24:30 +0100

    Increase update queues.

--- src/Constants/CompanionConstants.php
+++ src/Constants/CompanionConstants.php
@@ -5,6 +5,22 @@ namespace App\Constants;
 class CompanionConstants
 {
     const QUEUES = [
-        1,2,3,4,5,6,7,8,9,10
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        10,
+        11,
+        12,
+        13,
+        14,
+        15,
+
+        50, 60, 70
     ];
 }
